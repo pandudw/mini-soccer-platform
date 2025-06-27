6f8576dff590777d813561080f2b62f13830f37e
Pandu <nasrullah.pandud1@gmail.com>
2025-06-27 20:13:00 +0700

    fix: resolve validation error formatting bugs and enhance error messages

--- common/error.go
+++ common/error.go
@@ -1,8 +1,63 @@
 package common
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/validator"
+)
+
 type ValidationResponse struct {
 	Field   string `json:"field,omitempty"`
 	Message string `json:"message,omitempty"`
 }
 
 var ErrValidator = map[string]string{}
+
+func ErrValidationResponse(err error) (validationResponse []ValidationResponse) {
+	var fieldErrors validator.ValidationErrors
+	if errors.As(err, &fieldErrors) {
+		for _, err := range fieldErrors {
+			switch err.Tag() {
+			case "required":
+				validationResponse = append(validationResponse, ValidationResponse{
+					Field:   err.Field(),
+					Message: fmt.Sprintf("%s is required", err.Field()),
+				})
+			case "email":
+				validationResponse = append(validationResponse, ValidationResponse{
+					Field:   err.Field(),
+					Message: fmt.Sprintf("%s is not valid email", err.Field()),
+				})
+			default:
+				errValidator, ok := ErrValidator[err.Tag()]
+				if ok {
+					count := strings.Count(errValidator, "%s")
+					if count == 1 {
+						validationResponse = append(validationResponse, ValidationResponse{
+							Field:   err.Field(),
+							Message: fmt.Sprintf(errValidator, err.Field()),
+						})
+					} else if count == 2 {
+						validationResponse = append(validationResponse, ValidationResponse{
+							Field:   err.Field(),
+							Message: fmt.Sprintf(errValidator, err.Field(), err.Param()),
+						})
+					} else {
+						validationResponse = append(validationResponse, ValidationResponse{
+							Field:   err.Field(),
+							Message: fmt.Sprintf("validation failed for %s", err.Field()),
+						})
+					}
+				} else {
+					validationResponse = append(validationResponse, ValidationResponse{
+						Field:   err.Field(),
+						Message: fmt.Sprintf("validation failed for %s (rule: %s)", err.Field(), err.Tag()),
+					})
+				}
+			}
+		}
+	}
+	return
+}
